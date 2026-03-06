136e6c538ab7981c3a47e517f84d6441e2ae82b7
tomrtkx <91202225+tomrtkx@users.noreply.github.com>
2026-03-06 11:49:03 +0200

    Create test_1.py

---
+++ tests/test_1.py
@@ -0,0 +1,13 @@
+import unittest
+
+from func_to_test import test_func_1, test_func_2
+
+class test_test_1(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(test_func_1(2,3), 5)
+
+    def test_2(self):
+        self.assertEqual(test_func_2(5), 7)
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
